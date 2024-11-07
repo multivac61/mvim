64fe4a42235384b3790f85ba5708c0839a987514
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2024-11-07 16:09:44 +0000

    filetype detection heex, i wonder if I need it. also disable auto-format on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,22 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable aut spell-check and ncmp for text files set by lazyvimk
-vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("lazyvim_" .. "wrap_spell", { clear = true }),
-    pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.spell = false
-        require("cmp").setup.buffer({ enabled = false })
-    end,
-})
-
--- auto format on save
-vim.cmd([[autocmd BufWritePre *.nix lua vim.lsp.buf.format()]])
-
 -- Save on focus lost
 vim.api.nvim_create_autocmd("FocusLost", {
     command = "silent! wa",
     pattern = "*",
+})
+
+-- Filetype detection
+vim.filetype.add({
+    extension = {
+        heex = "heex",
+    },
 })
