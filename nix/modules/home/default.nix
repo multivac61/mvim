5adebb5b84aaabe49cb8337368c7f59c94f4430a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-25 10:54:45 +0000

    fix: another attempt

--- nix/modules/home/default.nix
+++ nix/modules/home/default.nix
@@ -15,7 +15,7 @@ in
     XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:-$HOME/.config}
     NVIM_APPNAME=''${NVIM_APPNAME:-nvim}
     mkdir -p $XDG_CONFIG_HOME/$NVIM_APPNAME
-    echo "${treesitter-grammars.rev}" > "$XDG_CONFIG_HOME/nvim/treesitter-rev"
+    echo "${treesitter-grammars.rev}" > "$XDG_CONFIG_HOME/$NVIM_APPNAME/treesitter-rev"
     if [[ -f $XDG_CONFIG_HOME/$NVIM_APPNAME/lazy-lock.json ]]; then
       if ! grep -q "${treesitter-grammars.rev}" "$XDG_CONFIG_HOME/$NVIM_APPNAME/lazy-lock.json"; then
         ${neovim}/bin/nvim --headless "+Lazy! update" +qa
@@ -23,7 +23,7 @@ in
     fi
   '';
 
-  xdg.dataFile."nvim".source = "${flake}";
-  xdg.dataFile."nvim".recursive = true;
-  xdg.dataFile."nvim/site/parser".source = treesitter-grammars;
+  home.file.".config/nvim".source = flake;
+  home.file.".config/nvim".recursive = true;
+  home.file.".config/nvim/site/parser".source = treesitter-grammars;
 }
